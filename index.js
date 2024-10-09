cbea9fdd761797d826a15f21f70ce6652cbe6392
pkcarlos <patricia.k.carlos@gmail.com>
2024-10-08 19:54:25 -0700

    add fronent production build

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const customFormat = (tokens, request, response) => {
 }
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(customFormat)) // configure morgan to show data sent in HTTP POST requests
 app.use(cors())
 
@@ -143,7 +144,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
